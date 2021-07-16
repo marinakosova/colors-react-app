62a05c3d876d5fc237a9084addfdc21a829201e3
marinakosova <marina.kosova@gmail.com>
2021-07-16 15:08:28 -0500

    feat: start styling PaletteList

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,21 +1,57 @@
 import React, { Component } from 'react';
 import MiniPalette from './MiniPalette';
 import { Link } from 'react-router-dom';
+import { withStyles } from '@material-ui/styles';
+
+const styles = {
+    root: {
+        height: "100%",
+        backgroundColor: "blue",
+        display: "flex",
+        alignItems: "flex-start",
+        justifyContent: "center"
+    },
+    container: {
+        width: "50%",
+        display: "flex",
+        alignItems: "flex-start",
+        flexDirection: "column",
+        flexWrap: "wrap"
+    },
+    nav: {
+        width: "100%",
+        display: "flex",
+        justifyContent: "space-between",
+        color: "white"
+    },
+    palettes: {
+        width: "100%",
+        boxSizing: "border-box",
+        display: "grid",
+        gridTemplateColumns: "repeat(3, 30%)",
+        gridGap: "5%"
+    }
+};
 
 class PaletteList extends Component {
     render() {
-        const { palettes } = this.props;
+        const { palettes, classes } = this.props;
         console.log(palettes)
         return (
-            <div>
-                <MiniPalette />
-                <h1>UI Flat Colors</h1>
-                {palettes.map(palette => (
-                    <MiniPalette {...palette} />
-                ))}
+            <div className={classes.root}>
+                <div className={classes.container}>
+                    <nav className={classes.nav}>
+                        <h1>UI Flat Colors</h1>
+                    </nav>
+                    <div className={classes.palettes}>
+                        {palettes.map(palette => (
+                            <MiniPalette {...palette} />
+                        ))}
+                    </div>
+                </div>
             </div>
         );
     }
 }
 
-export default PaletteList;
+export default withStyles(styles)(PaletteList);
