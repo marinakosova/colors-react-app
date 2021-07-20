1a1ecc3704010bc1cd341e1ed16e725196b138d5
marinakosova <marina.kosova@gmail.com>
2021-07-20 15:35:00 -0500

    refactor: finish refactor of ColorBox css

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -9,14 +9,33 @@ const styles = {
     colorBox: {
         width: "20%",
         height: props => props.showingFullPalette ? "25%" : "50%",
-        margin: "0 auto",
+        margin: "0px auto",
         marginBottom: "-3.5px",
         display: "inline-block",
         position: "relative",
         cursor: "pointer",
         "&:hover button": {
-            opacity: 1
+            opacity: "1"
         }
+    },
+    boxContent: {
+        width: "100%",
+        position: "absolute",
+        left: "0px",
+        bottom: "10px",
+        padding: "10px",
+        color: "black",
+        letterSpacing: "1px",
+        textTransform: "uppercase",
+        fontSize: "12px"
+    },
+    copyOverlay: {
+        width: "100%",
+        height: "100%",
+        transition: "transform 0.6s ease-in-out",
+        opacity: "0",
+        zIndex: "0",
+        transform: "scale(0.1)"
     },
     copyText: {
         color: props => chroma(props.background).luminance() >= 0.7 ? "black" : "white"
@@ -57,7 +76,49 @@ const styles = {
         textTransform: "uppercase",
         border: "none",
         textDecoration: "none",
-        opacity: 0
+        opacity: "0"
+    },
+    showOverlay: {
+        position: "absolute",
+        opacity: "1",
+        transform: "scale(50)",
+        zIndex: "10"
+    },
+    copyMessage: {
+        position: "fixed",
+        top: "0px",
+        bottom: "0px",
+        left: "0px",
+        right: "0px",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        fontSize: "4rem",
+        transform: "scale(0.1)",
+        opacity: "0",
+        color: "white",
+        "& h1": {
+            width: "100%",
+            fontWeight: "400",
+            textShadow: "1px 2px black",
+            background: "rgba(255, 255, 255, 0.2)",
+            textAlign: "center",
+            marginBottom: "0px",
+            padding: "1rem",
+            textTransform: "uppercase"
+        },
+        "& p": {
+            fontSize: "2rem",
+            fontWeight: "100"
+        }
+    },
+    showMessage: {
+        opacity: "1",
+        transform: "scale(1)",
+        zIndex: "25",
+        transition: "all 0.4s ease-in-out",
+        transitionDelay: "0.3s"
     }
 };
 
@@ -81,13 +142,13 @@ class ColorBox extends Component {
             <CopyToClipboard text={background} onCopy={this.changeCopyState}>
                 <div style={{ background }} className={classes.colorBox}>
                     <div style={{ background }}
-                        className={`copy-overlay ${copied && "show"}`} />
-                    <div className={`copy-msg ${copied && "show"}`}>
+                        className={`${classes.copyOverlay} ${copied && classes.showOverlay}`} />
+                    <div className={`${classes.copyMessage} ${copied && classes.showMessage}`}>
                         <h1>copied!</h1>
                         <p className={classes.copeText}>{this.props.background}</p>
                     </div>
-                    <div className="copy-container">
-                        <div className="box-content">
+                    <div>
+                        <div className={classes.boxContent}>
                             <span className={classes.colorName}>{name}</span>
                         </div>
                         <button className={classes.copyButton}>
