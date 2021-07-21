26719fc102c3d970123e1c5dcf08ade86b8e234b
marinakosova <marina.kosova@gmail.com>
2021-07-21 13:05:04 -0500

    refactor: move out styles

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -2,36 +2,7 @@ import React, { Component } from 'react';
 import MiniPalette from './MiniPalette';
 import { Link } from 'react-router-dom';
 import { withStyles } from '@material-ui/styles';
-
-const styles = {
-    root: {
-        height: "100vh",
-        backgroundColor: "blue",
-        display: "flex",
-        alignItems: "flex-start",
-        justifyContent: "center"
-    },
-    container: {
-        width: "50%",
-        display: "flex",
-        alignItems: "flex-start",
-        flexDirection: "column",
-        flexWrap: "wrap"
-    },
-    nav: {
-        width: "100%",
-        display: "flex",
-        justifyContent: "space-between",
-        color: "white"
-    },
-    palettes: {
-        width: "100%",
-        boxSizing: "border-box",
-        display: "grid",
-        gridTemplateColumns: "repeat(3, 30%)",
-        gridGap: "5%"
-    }
-};
+import styles from './styles/PaletteListStyles';
 
 class PaletteList extends Component {
     goToPalette(id) {
---
+++ src/styles/PaletteFooterStyles.js
@@ -0,0 +1,14 @@
+export default {
+    paletteFooter: {
+        backgroundColor: "#fff",
+        height: "5vh",
+        display: "flex",
+        justifyContent: "flex-end",
+        alignItems: "center",
+        fontWeight: "bold"
+    },
+    emoji: {
+        fontSize: "1.5rem",
+        margin: "0px 1rem"
+    }
+}
